5945f0f8b334ff910bdf7855b0bdf0d2b3fb4846
V01DF0G <V01DF0G@users.noreply.github.com>
2021-06-14 15:59:32 +0300

    RapidAPI bug fix by adding bloat again

--- src/main/java/viko/eif/lt/dfultinavcius/freeflight/AirportInfo/AirportInfoController.java
+++ src/main/java/viko/eif/lt/dfultinavcius/freeflight/AirportInfo/AirportInfoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import viko.eif.lt.dfultinavcius.freeflight.BackgroundMethods;
-import viko.eif.lt.dfultinavcius.freeflight.ExternalAPI.ExternalAPI;
 
 import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
