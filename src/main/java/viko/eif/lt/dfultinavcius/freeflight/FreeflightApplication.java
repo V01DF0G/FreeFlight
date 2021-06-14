4e64c4a6014ef1f8914baaa558e311fe2c72464d
V01DF0G <V01DF0G@users.noreply.github.com>
2021-06-14 09:37:27 +0300

    Retrying server side deployment part 11

--- src/main/java/viko/eif/lt/dfultinavcius/freeflight/FreeflightApplication.java
+++ src/main/java/viko/eif/lt/dfultinavcius/freeflight/FreeflightApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class FreeflightApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
