111378d77a474101402b08a3e808221d5255ebc3
V01DF0G <V01DF0G@users.noreply.github.com>
2021-06-14 09:28:26 +0300

    Retrying server side deployment part 9

--- src/main/java/viko/eif/lt/dfultinavcius/freeflight/FreeflightApplication.java
+++ src/main/java/viko/eif/lt/dfultinavcius/freeflight/FreeflightApplication.java
@@ -3,18 +3,21 @@ package viko.eif.lt.dfultinavcius.freeflight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class FreeflightApplication {
+public class FreeflightApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FreeflightApplication.class, args);
     }
 
     @GetMapping("/")
+    @ResponseBody
     public String hello()
     {
         return "Hello to the FreeFlight REST API !";
