7eeb309151cc5224e164c59de182c588dcef4924
V01DF0G <V01DF0G@users.noreply.github.com>
2021-06-14 15:56:43 +0300

    RapidAPI bug fix by adding bloat again

--- src/main/java/viko/eif/lt/dfultinavcius/freeflight/AirportInfo/AirportInfoController.java
+++ src/main/java/viko/eif/lt/dfultinavcius/freeflight/AirportInfo/AirportInfoController.java
@@ -46,18 +46,28 @@ public class AirportInfoController
     public AirportInfo getNearestAirport(HttpServletRequest request) throws IOException, InterruptedException {
         var ip = BackgroundMethods.getClientIpAddress(request);
 
+        HttpRequest ipGeoReq = HttpRequest.newBuilder()
+                .uri(URI.create("https://free-geo-ip.p.rapidapi.com/json/" + ip))
+                .header("x-rapidapi-key", "a1aef2c13cmsh1db981720bb680ep131df9jsn4718336aca9c")
+                .header("x-rapidapi-host", "free-geo-ip.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> ipGeoResp = HttpClient.newHttpClient().send(ipGeoReq, HttpResponse.BodyHandlers.ofString());
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode ipGeoNode = mapper.readTree(ExternalAPI.getIpGeoreq(ip).body());
-        String LongLatiparams = ipGeoNode.get("latitude").asText() + "%2C" + ipGeoNode.get("longitude").asText();
-        String country_code = ipGeoNode.get("country_code").asText();
+        JsonNode ipGeoNode = mapper.readTree(ipGeoResp.body());
 
-        System.out.println(LongLatiparams);
-        System.out.println(country_code);
+        String LongLatipar = ipGeoNode.get("latitude").asText() + "%2C" + ipGeoNode.get("longitude").asText();
 
-        JsonNode geoServiceNode = mapper.readTree(ExternalAPI.getGeoServicereq(LongLatiparams,country_code).body());
+        HttpRequest GeoServicereq = HttpRequest.newBuilder()
+                .uri(URI.create("https://geo-services-by-mvpc-com.p.rapidapi.com/airports?language=en&radius=50&location=" + LongLatipar + "&countrycode=" + ipGeoNode.get("country_code").asText()))
+                .header("x-rapidapi-key", "a1aef2c13cmsh1db981720bb680ep131df9jsn4718336aca9c")
+                .header("x-rapidapi-host", "geo-services-by-mvpc-com.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> GeoServiceresp = HttpClient.newHttpClient().send(GeoServicereq, HttpResponse.BodyHandlers.ofString());
+
+        JsonNode geoServiceNode = mapper.readTree(GeoServiceresp.body());
         AirportInfo airportInfo = new AirportInfo();
-
-        System.out.println(ExternalAPI.getGeoServicereq(LongLatiparams,country_code).body());
 
         airportInfo.setAirportName(geoServiceNode.get("data").get(0).get("name").asText());
         airportInfo.setCity(geoServiceNode.get("data").get(0).get("cityname").asText());
